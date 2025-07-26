8bd5ab6aed3352bc9595529a93508d7aa8353a7c
Deibipgm <deibiss10@gmail.com>
2025-07-26 07:13:20 -0300

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from routes.api_routes import api_bp
 from routes.irrigacao_routes import irrigacao_bp
 from routes.dashboard_routes import dashboard_bp
 from routes.relatorios_routes import relatorios_bp
-
-
 import sys, os
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
@@ -16,22 +14,23 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 app = Flask(__name__)
 app.template_folder = "templates"
 
-# ✅ SECRET_KEY precisa estar ANTES de tudo que usa sessão
 app.secret_key = "irrigacao2025@flask!"
 
-# ✅ Configurações do banco
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Marimel100%@localhost/db_irrigacao'
+# ✅ Aqui usamos o valor da variável de ambiente do Render
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# ✅ Inicialização do banco
 db.init_app(app)
 
-# ✅ Página inicial redireciona para o dashboard
+@app.route('/')
+def index():
+    return 'Bem-vindo ao sistema Irrigação Flask + Render!'
+
 @app.route("/home")
 def home():
     return redirect(url_for('dashboard.dashboard'))
 
-# ✅ Blueprints
+# Blueprints
 app.register_blueprint(fazenda_bp)
 app.register_blueprint(cultura_bp)
 app.register_blueprint(projeto_bp)
@@ -40,6 +39,6 @@ app.register_blueprint(irrigacao_bp)
 app.register_blueprint(relatorios_bp)
 app.register_blueprint(dashboard_bp)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
